06bd191b191e01878cfddfa0603dbacd2fe2b9f5
Yassine <160136312+Yasssa02@users.noreply.github.com>
2025-02-06 12:17:44 +0100

    Atelier 2 partie 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Hello from './Components/Hello'
 import Form from './Components/Form'
+import ComponentClass from './Components/ComponentClass';
+import ComponentFonct from './Components/ComponentFonct';
+import ComponentEffect from './Components/ComponentEffect';
+import ComponentTimer from './Components/ComponentTimer';
+import ColorBox from './Components/ColorBox';
+import Counter from './Components/Counter';
+import ListManager from './Components/ListManager.JSX';
+import NoteManager from './Components/NoteManager';
+import TodoList from './Components/TodoList'
+import Event from './Components/Event'
+import Events from './Components/Events'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,8 +43,45 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
       <Hello/>
+      <h1>Atelier 0</h1>
+      
       <Form labelle="FirstName : " type="text" namee="FirstName" name="Firstname"/>
       <Form labelle="LastName : "/>
+      <div className="App">
+      <ComponentClass />
+     </div>
+     <div className="App">
+      <ComponentFonct />
+     </div>
+     <div className="App">
+      <ComponentEffect />
+     </div>
+     <div className="App">
+      <ComponentTimer />
+     </div>
+     <div className="App">
+      <ColorBox />
+     </div>
+     <div className="App">
+      <Counter />
+     </div>
+     <div className="App">
+      <ListManager />
+     </div>
+     <div className="App">
+      <NoteManager />
+     </div>
+     <div className="App">
+      <TodoList />
+     </div>
+     <h1>Atelier Composant Fonctionnels</h1>
+     <div className="App">
+      <h1 className="text-center my-4">Event Management System</h1>
+      <Events/>
+    </div>
+     
+  
+      
     </>
   )
 }
